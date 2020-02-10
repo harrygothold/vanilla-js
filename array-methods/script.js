12a52643cbb58baf8cfa5394f2228a851999bce0
Harry Gothold <harry.gothold@kurtgeiger.com>
2020-02-10 12:58:09 +0000

    Outputted users into the DOM

---
+++ array-methods/script.js
@@ -0,0 +1,49 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculatWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+// fetch random user and add money
+const getRandomUser = async () => {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+  addData(newUser);
+};
+
+// Add new obj to data arr
+const addData = obj => {
+  data.push(obj);
+  updateDOM();
+};
+
+// update DOM
+const updateDOM = (providedData = data) => {
+    // clear main div
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> £${formatMoney(item.money)}`;
+        main.appendChild(element);
+    })
+}
+
+// Format number as money
+const formatMoney = number => {
+ return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
+}
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+addUserBtn.addEventListener('click', getRandomUser);
