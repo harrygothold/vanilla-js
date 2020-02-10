fe9a29265c4637d97314c6ebc3077001ad4c1eca
Harry Gothold <harrygot@hotmail.com>
2020-02-10 21:38:19 +0000

    Added speech recognition game

---
+++ speech-recognition/script.js
@@ -0,0 +1,63 @@
+const msgEl = document.getElementById("msg");
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 100) + 1;
+};
+
+const randomNum = getRandomNumber();
+
+console.log(randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+const writeMessage = msg => {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class='box'>${msg}</span>
+    `;
+};
+
+const checkNumber = msg => {
+  const num = +msg;
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += "<div>That is not a valid number</div>";
+    return;
+  }
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+    return;
+  }
+
+  if (num === randomNum) {
+    document.body.innerHTML = `
+    <h2>Congrats! You have guessed the number! <br><br><br> It was ${num}</h2>
+    <button class='play-again' id='play-again'>Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += "<div>GO LOWER!</div>";
+  } else {
+    msgEl.innerHTML += "<div>GO HIGHER!</div>";
+  }
+};
+
+const onSpeak = e => {
+  const msg = e.results[0][0].transcript;
+  writeMessage(msg);
+  checkNumber(msg);
+};
+
+// Speak result
+recognition.addEventListener("result", onSpeak);
+recognition.addEventListener("end", () => recognition.start());
+
+document.body.addEventListener("click", e => {
+  if (e.target.id === "play-again") {
+    window.location.reload();
+  }
+});
